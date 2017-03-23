88a57764e970ed4201787be2120ef65a9d39310a
Taras Zlotnikov <taras.zlotnikov@nevosoft.ru>
2017-03-23 13:34:43 +0300

    android

---
+++ src/android/DigitalStar.java
@@ -0,0 +1,226 @@
+package org.apache.cordova.plugin;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import static java.lang.System.*;
+import android.content.Context;
+import cards.digitalstar.digitalstarcards.*;
+import cards.digitalstar.digitalstarcards.DSResponses.*;
+
+
+class AppStartCallbackImp implements DSCards.AppStartCallback {
+    
+    private CallbackContext callbackContext;
+    
+    public AppStartCallbackImp(CallbackContext cc) {
+        callbackContext = cc;
+    }
+    
+    public void onCompleted(AppStartResponse result) {
+        if(result == AppStartResponse.FinishedSuccessfully) {
+            callbackContext.success();
+        } else {
+            callbackContext.error(result.getNumericType());
+        }
+    }
+}
+
+class GetClusterDataCallbackImp implements DSCards.GetClusterDataCallback {
+    
+    private CallbackContext callbackContext;
+    
+    public GetClusterDataCallbackImp(CallbackContext cc) {
+        callbackContext = cc;
+    }
+    
+    public void onCompleted(GetClusterDataResponse result) {
+        if(result == GetClusterDataResponse.FinishedSuccessfully) {
+            callbackContext.success(result.getJson());
+        } else {
+            callbackContext.error(result.getNumericType());
+        }
+    }
+}
+
+class OpenOfferCallbackImp implements DSCards.OpenOfferCallback {
+    
+    private CallbackContext callbackContext;
+    
+    public OpenOfferCallbackImp(CallbackContext cc) {
+        callbackContext = cc;
+    }
+    
+    public void onCompleted(OpenOfferResponse result) {
+        if(result == OpenOfferResponse.FinishedSuccessfully) {
+            callbackContext.success();
+        } else {
+            callbackContext.error(result.getNumericType());
+        }
+    }
+}
+
+class OpenClusterCallbackImp implements DSCards.OpenClusterCallback {
+    
+    private CallbackContext callbackContext;
+    
+    public OpenClusterCallbackImp(CallbackContext cc) {
+        callbackContext = cc;
+    }
+    
+    public void onCompleted(OpenClusterResponse result) {
+        if(result == OpenClusterResponse.FinishedSuccessfully) {
+            callbackContext.success();
+        } else {
+            callbackContext.error(result.getNumericType());
+        }
+    }
+}
+
+class CheckDSCardsInstalledCallbackImp implements DSCards.CheckDSCardsInstalledCallback {
+ 
+    private CallbackContext callbackContext;
+    
+    public CheckDSCardsInstalledCallbackImp(CallbackContext cc) {
+        callbackContext = cc;
+    }
+    
+    public void onCompleted(CheckDSCardsInstalledResponse result) {
+        callbackContext.success(result.getNumericType()); 
+    } 
+}
+
+
+/**
+* This class echoes a string called from JavaScript.
+*/
+public class DigitalStar extends CordovaPlugin {
+
+private DSCards ds;
+private String serviceId;
+private String apiKey;
+private String clusterId;
+private Context context; 
+
+@Override
+public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    try {
+        if (action.equals("init")) {
+            return init(callbackContext);    
+        }  else if (action.equals("setServer")) {
+            return setServer(callbackContext, args.getInt(0));    
+        } else if (action.equals("appStart")) {
+            return appStart(callbackContext);
+        } else if (action.equals("getClusterData")) {
+            return getClusterData(callbackContext, args.getBoolean(0));
+        } else if (action.equals("setLocale")) {
+            return setLocale(callbackContext, args.getString(0), args.getString(1));
+        } else if (action.equals("setActivateOfferCallBackUrl")) {
+            return setActivateOfferCallBackUrl(callbackContext, args.getString(0));
+        }  else if (action.equals("userSeeDSOffers")) {
+            return userSeeDSOffers(callbackContext);
+        } else if (action.equals("checkDSCardsInstalled")) {
+            return checkDSCardsInstalled(callbackContext);
+        } else if (action.equals("openOffer")) {
+            return openOffer(callbackContext, args.getString(0));
+        } else if (action.equals("openCluster")) {
+            return openCluster(callbackContext);
+        } else if (action.equals("openDSCards")) {
+            return openDSCards(callbackContext);
+        } else {
+            return false;
+        }
+    } catch (Exception e) {
+        callbackContext.error(1001);  
+    }
+    return true;
+}
+
+private boolean init(CallbackContext callbackContext) {
+    ds = DSFactory.getDSCards();
+    context = this.cordova.getActivity().getApplicationContext(); 
+    serviceId = getParameter("service_id");
+    apiKey = getParameter("api_key");
+    clusterId = getParameter("cluster_id");
+    InitResponse response = ds.init(context, serviceId, apiKey, clusterId);
+    if(response == InitResponse.FinishedSuccessfully) {
+        callbackContext.success();
+    } else {
+        callbackContext.error(response.getNumericType());
+    }
+    return true;
+}    
+
+private boolean setServer(CallbackContext callbackContext, int type) {  
+    ds.setServer(ServerType.fromValue(type));
+    callbackContext.success();
+    return true;
+}
+        
+private boolean appStart (CallbackContext callbackContext) {
+    ds.appStart(new AppStartCallbackImp(callbackContext));
+    return true;
+}
+
+private boolean getClusterData (CallbackContext callbackContext, boolean isFirstStart) {
+    ds.getClusterData(isFirstStart, new GetClusterDataCallbackImp(callbackContext));
+    return true;
+}
+
+private boolean setLocale (CallbackContext callbackContext, String language, String country) {
+    ds.setLocale(language, country);
+    callbackContext.success();
+    return true;
+}
+
+private boolean setActivateOfferCallBackUrl (CallbackContext callbackContext, String url) {
+    ds.setActivateOfferCallBackUrl(url);
+    callbackContext.success();
+    return true;
+}
+
+private boolean userSeeDSOffers (CallbackContext callbackContext) {
+    ds.userSeeDSOffers();
+    callbackContext.success();
+    return true;
+}
+
+private boolean checkDSCardsInstalled (CallbackContext callbackContext) {
+    ds.checkDSCardsInstalled(new CheckDSCardsInstalledCallbackImp(callbackContext));
+    return true;
+}
+
+private boolean openOffer(CallbackContext callbackContext, String offerID) {
+    ds.openOffer(offerID, new OpenOfferCallbackImp(callbackContext));
+    return true;
+}
+
+private boolean openCluster (CallbackContext callbackContext) {
+    ds.openCluster(new OpenClusterCallbackImp(callbackContext));
+    return true;
+}
+
+private boolean openDSCards (CallbackContext callbackContext) {
+    ds.openDSCards(new OpenClusterCallbackImp(callbackContext));
+    return true;
+}
+
+private String getParameter(String name) {
+    int apiKeyFromParam = cordova.getActivity().getResources().getIdentifier(name + "_param", "string", cordova.getActivity().getPackageName());
+    String ret = "";
+
+    if (apiKeyFromParam > 0) {
+        ret = cordova.getActivity().getString(apiKeyFromParam);
+        if (ret.length() > 0) {
+            return ret;
+        }
+    }
+
+    int apiKey = cordova.getActivity().getResources().getIdentifier(name, "string", cordova.getActivity().getPackageName());
+    return cordova.getActivity().getString(apiKey);
+}
+    
+}
